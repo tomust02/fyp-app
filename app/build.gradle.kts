eb5198bb5a46a43e7ff1fb4872b8c23b5cfcf51b
tomust02 <swsoae@connect.ust.hk>
2024-12-17 01:16:50 +0800

    so v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    //id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,7 +49,28 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
+    // TODO: Add the dependencies for any other Firebase products you want to use
+    // See https://firebase.google.com/docs/android/setup#available-libraries
+    // For example, add the dependencies for Firebase Authentication and Cloud Firestore
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
 }
--- app/src/main/java/com/example/fypapp/MainActivity.kt
+++ app/src/main/java/com/example/fypapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.fypapp
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,52 +12,138 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 import com.example.fypapp.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 
-class MainActivity : AppCompatActivity() {
+data class HeartRateData(val heart_rate: String = "")
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+class MainActivity() : AppCompatActivity() {
+
+
+//    private lateinit var appBarConfiguration: AppBarConfiguration
+     private lateinit var binding: ActivityMainBinding
+    
+    private  lateinit var databaseRef : DatabaseReference
+    private lateinit var heartRateTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //such that you can show text view
+        //setContentView(R.layout.activity_main);
 
+        //real time
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        databaseRef = FirebaseDatabase.getInstance().getReference("heart_rate")
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+
+        // Initialize the TextView using View Binding
+        // This assumes your TextView in the layout has the ID heart_rate
+        //val heartRateTextView = binding.heartRate
+
+//        heartRateTextView = findViewById(R.id.heart_rate)
+
+        //test firebase connection
+//        databaseRef = FirebaseDatabase.getInstance().getReference("test")
+//
+//        binding.hello.setOnClickListener{
+//            databaseRef.setValue("test123 ok")
+//                .addOnSuccessListener {
+//                    Toast.makeText(this,"data stroed seccessfully", Toast.LENGTH_LONG)
+//                }
+//        }
+
+        readdate()
+
+//
+//        firebaseRef = FirebaseDatabase.getInstance().getReference()
+//        binding.heartRate.setOnClickListener{}
+
+//        //Firestore
+//        val db=FirebaseFirestore.getInstance()
+//        //declare the animation
+//        val heart_rate = findViewById(R.id.heart_rate) as TextView
+//
+//        val docRef = db.collection("sensors").document("user")
+//        docRef.get()
+//            .addOnSuccessListener { document ->
+//                if (document != null) {
+//                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+//
+//                    heart_rate.text = document.getString("heart_rate")
+//                } else {
+//                    Log.d("noexist", "No such document")
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.d("errordb", "get failed with ", exception)
+//            }
+
+    }
+
+    private fun readdate(){
+        val databaseRef = FirebaseDatabase.getInstance().getReference("heart_rate")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val heartRateData = dataSnapshot.getValue(HeartRateData::class.java)
+                binding.heartRate.text = heartRateData?.let {
+                    "Heart Rate: ${it.heart_rate} bpm"
+                } ?: "No data available"
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("FirebaseError", "loadPost:onCancelled", databaseError.toException())
+                binding.heartRate.text = "Error: ${databaseError.message}"
+            }
         }
+
+        databaseRef.addValueEventListener(postListener)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+
+
+    // look at this section
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        val inflater: MenuInflater = menuInflater
+//        inflater.inflate(R.menu.my_menu, menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        return when (item.itemId) {
+//            R.id.action_settings -> true
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        return navController.navigateUp(appBarConfiguration)
+//                || super.onSupportNavigateUp()
+//    }
 }
+
